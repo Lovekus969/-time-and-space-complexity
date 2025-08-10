fd320849bb1105684b6f7611711e1dd8aae3febf
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-09 23:54:32 -0400

    Update Solution.java

--- VariationOfDSA/Arrays/Solution.java
+++ VariationOfDSA/Arrays/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int maxSoFar = nums[0];  // To store the maximum sum found so far
         int currentSum = nums[0]; // To store the current subarray sum
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {     //KEEP IN MIND AS IT START FROM  SECOND POSTION BECAUSE FIRST WE ALREDY FIXED .
             // Either extend the current subarray or start a new one
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             // Update maxSoFar if currentSum is greater
