01157938028d99fc064f8090f3a75a5161c48454
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-09 23:53:49 -0400

    Create Solution.java

---
+++ VariationOfDSA/Arrays/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];  // To store the maximum sum found so far
+        int currentSum = nums[0]; // To store the current subarray sum
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either extend the current subarray or start a new one
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            // Update maxSoFar if currentSum is greater
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
+        return maxSoFar;
+    }
+}
