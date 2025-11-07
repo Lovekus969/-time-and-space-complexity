e8ccdf60546540df4bdfb66267c3dec0d359731a
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-07 00:02:53 -0500

    Add linear.c to print line numbers in a loop

---
+++ Complexity/linear.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i;
+    for(i =0;i<17;i++){
+        printf("Hello bhavneet this is the line number   ", i ," in C code ");
+    }
+
+    return 0;
+}
