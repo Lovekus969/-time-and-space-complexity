fe2a40c47edefa7a8d236e96b06abf5c0018f376
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-07 00:19:53 -0500

    Add Nested.c to print a decreasing pattern of stars

---
+++ Complexity/Nested.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i;
+    for(i =10;i>=1;i--){
+        for(int j=1;j<=i;j++){
+        printf("  *  " );
+        }
+        printf("\n");
+       
+    }
+
+    return 0;
+}
